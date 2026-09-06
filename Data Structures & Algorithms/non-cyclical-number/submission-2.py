424259aab36bc63074583ac15b6a666372d6ae61
Mindy Jen <jencmhep@gmail.com>
2026-09-06 15:39:04 +0800

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def helper(self, n: int) -> int:
+
+        ans = 0
+
+        while n > 0:
+            ans += (n % 10) ** 2
+            n //= 10
+
+        return ans
+
+    def isHappy(self, n: int) -> bool:
+        
+        ans = list()
+
+        while n != 1:
+            n = self.helper(n)
+            print(n)
+            if n not in ans: ans.append(n)
+            else: return False
+            
+
+        return True
+        
