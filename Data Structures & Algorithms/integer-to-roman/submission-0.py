b487a6d12cf69919eaa33093565e98623525a2dc
Mindy Jen <jencmhep@gmail.com>
2026-09-09 08:53:53 +0800

    Add: integer-to-roman - submission-0

---
+++ Data Structures & Algorithms/integer-to-roman/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+        mp = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
+
+        ans = ''
+
+        for k, v in mp.items():
+            if num < 1: break
+            ans += v * (num // k)
+            num %= k
+
+        return ans
+
+
+
